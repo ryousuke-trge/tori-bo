831b0349c553b07ff98b1f23c828277f8486f90b
ryousuke971003-cmyk <ryousuke971003@gmail.com>
2026-04-09 09:54:40 +0900

    Update bottom navigation home icon to use calendar pencil image

--- src/components/BottomNav.ts
+++ src/components/BottomNav.ts
@@ -9,8 +9,7 @@ export function renderBottomNav(container: HTMLElement, currentHash: string) {
 
   container.innerHTML = `
     <a href="#/" class="${baseClass} ${isHome ? activeClass : inactiveClass}">
-      <span class="text-xl mb-1">📅</span>
-      <span>ホーム</span>
+      <img src="/home.png" alt="ホーム" class="w-11 h-11 object-contain transition-all ${isHome ? 'opacity-100 scale-105' : 'opacity-50 grayscale'}" />
     </a>
     <a href="#/stats" class="${baseClass} ${isStats ? activeClass : inactiveClass}">
       <span class="text-xl mb-1">📊</span>
